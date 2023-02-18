7ae7b16247b762654de6bb29b58fbb9f3c795474
jazmin saleh peña <jazminasaleh@gmail.com>
2023-02-18 16:21:38 -0500

    mapa descripcion

--- lib/src/pages/publicaciones/descripcion_publicaciones.dart
+++ lib/src/pages/publicaciones/descripcion_publicaciones.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:app_arriendosu/src/pages/publicaciones/galeria_imagenes.dart';
 import 'package:flutter/material.dart';
 import 'package:app_arriendosu/src/utils/colors.dart' as utils;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../ubicacion/ubicacion.dart';
 
 //*pagina descripcion de publicaciones
-class DescripcionPublicaciones extends StatelessWidget {
+class DescripcionPublicaciones extends StatefulWidget {
   List slides;
   String direccion;
   String barrio;
@@ -23,15 +28,32 @@ class DescripcionPublicaciones extends StatelessWidget {
       required this.nombreDeatlles,
       required this.iconosRestricciones,
       required this.nombreRestricciones,
-      required this.descripcion
-      }
-  );
+      required this.descripcion});
 
   @override
+  State<DescripcionPublicaciones> createState() => _DescripcionPublicacionesState();
+}
+
+class _DescripcionPublicacionesState extends State<DescripcionPublicaciones> {
+  Completer<GoogleMapController> _controller = Completer();
+  UbicacionController _ubicacionController = new UbicacionController();
+  @override
   Widget build(BuildContext context) {
+     final CameraPosition puntoInicial = CameraPosition(
+      target: LatLng(5.703595, -72.943689),
+      zoom: 15,
+      tilt: 60
+    );
+    Set<Marker> markers = new Set<Marker>();
+    markers.add(
+      new Marker(
+        markerId: MarkerId('universidad'),
+        position: LatLng(5.704476, -72.941981),
+      ),
+    );
     return Container(
         child: Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
       child: Column(
         children: [
           const SizedBox(
@@ -41,7 +63,7 @@ class DescripcionPublicaciones extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Text(
-                direccion,
+                widget.direccion,
                 style: const TextStyle(
                     fontSize: 25,
                     color: utils.Colors.blanco,
@@ -53,7 +75,7 @@ class DescripcionPublicaciones extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Text(
-                barrio,
+                widget.barrio,
                 style: const TextStyle(
                     fontSize: 18,
                     color: utils.Colors.blanco,
@@ -75,7 +97,7 @@ class DescripcionPublicaciones extends StatelessWidget {
               const Text(
                 'Galeria',
                 style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 25,
                     color: utils.Colors.blanco,
                     fontWeight: FontWeight.w500),
               ),
@@ -84,7 +106,7 @@ class DescripcionPublicaciones extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => GaleriaImagenes()),
+                          builder: (context) => const GaleriaImagenes()),
                     );
                   },
                   child: const Text(
@@ -93,50 +115,86 @@ class DescripcionPublicaciones extends StatelessWidget {
                   ))
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 10,
           ),
-          _galeria(slides: slides),
-          SizedBox(
+          _galeria(slides: widget.slides),
+          const SizedBox(
             height: 20,
           ),
           Row(
-            children: [
+            children: const [
               Text(
                 'Detalles',
                 style: TextStyle(
-                    fontSize: 20,
-                    color: utils.Colors.blanco,
-                    fontWeight: FontWeight.w500),
+                  fontSize: 25,    
+                  color: utils.Colors.blanco,
+                  fontWeight: FontWeight.w500),
               ),
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 10,
           ),
           _detalles(
-              iconoDetalles: iconosDetalles, nombreDetalles: nombreDeatlles),
+              iconoDetalles: widget.iconosDetalles, nombreDetalles: widget.nombreDeatlles),
           Row(
-            children: [
+            children: const[
               Text(
                 'Restricciones',
                 style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 25,
                     color: utils.Colors.rojo,
                     fontWeight: FontWeight.w500),
               ),
             ],
           ),
-           SizedBox(
+          const SizedBox(
             height: 10,
           ),
           _restricciones(
-            iconoRestricciones: iconosRestricciones,
-            nombreRetricciones: nombreRestricciones,
+            iconoRestricciones: widget.iconosRestricciones,
+            nombreRetricciones: widget.nombreRestricciones,
           ),
-          SizedBox(
-            height: 20,
+          Padding(
+            padding: EdgeInsets.only(right: MediaQuery.of(context).size.width*0.53),
+            child: const Text(
+              'Descripción',
+              style: TextStyle(
+                fontSize: 25,
+                color: utils.Colors.blanco,
+                fontWeight: FontWeight.w500),
+               
+              ),
           ),
+          _descripcion(descripcion: widget.descripcion),
+          const SizedBox(height: 40,),
+          Row(
+            children: const [
+              Text(
+                'Ubicación',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: utils.Colors.blanco,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+           const SizedBox(height: 20,),
+          Container(
+            height: 350,
+            width: 350,
+            child: GoogleMap(
+              myLocationButtonEnabled: true,
+              markers: markers,
+              mapType: MapType.normal,
+              initialCameraPosition: puntoInicial,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+          const SizedBox(height: 30,),
         ],
       ),
     ));
@@ -159,19 +217,19 @@ class _galeria extends StatelessWidget {
       child: Column(
         children: [
           Expanded(
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: slides.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 100,
-                      width: 140,
-                      margin: EdgeInsets.symmetric(horizontal: 5),
-                      child: slides[index],
-                    );
-                  }
-              )
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: slides.length,
+              itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 100,
+                width: 140,
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                child: slides[index],
+              );
+              }
             )
+          )
         ],
       ),
     );
@@ -196,26 +254,26 @@ class _detalles extends StatelessWidget {
       child: Column(
         children: [
           Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: iconoDetalles.length,
-                itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 100,
-                  width: 90,
-                  margin: EdgeInsets.symmetric(horizontal: 5),
-                  child: Row(
-                    children: [
-                      Column(
-                        children: [
-                          iconoDetalles[index],
-                          const SizedBox(height: 5,),
-                          nombreDetalles[index]
-                        ],
-                      )
-                    ],
-                  )
-                );
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: iconoDetalles.length,
+              itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 100,
+                width: 90,
+                margin: const EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        iconoDetalles[index],
+                        const SizedBox(height: 5,),
+                        nombreDetalles[index]
+                      ],
+                    )
+                  ],
+                )
+              );
               }
             )
           )
@@ -243,14 +301,14 @@ class _restricciones extends StatelessWidget {
       child: Column(
         children: [
           Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: iconoRestricciones.length,
-                itemBuilder: (BuildContext context, int index) {
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: iconoRestricciones.length,
+              itemBuilder: (BuildContext context, int index) {
                 return Container(
                   height: 100,
                   width: 90,
-                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
                   child: Row(
                     children: [
                       Column(
@@ -261,11 +319,34 @@ class _restricciones extends StatelessWidget {
                         ],
                       )
                     ],
-                  )
+                  ) 
                 );
               }
             )
           )
+        ],
+      ),
+    );
+  }
+}
+
+class _descripcion extends StatelessWidget {
+  _descripcion({
+   required this.descripcion,
+  });
+  String descripcion;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+         Column(
+          children: [
+            SizedBox(height: 10,),
+            Text(descripcion, style: TextStyle(fontSize: 18, color: utils.Colors.blanco), textAlign: TextAlign.justify,)
+          ],
+         )
         ],
       ),
     );
